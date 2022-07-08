081af458a614e81164b9c6bb31c2c1b28201ecd8
jihyun-jeon <jhplus13@naver.com>
2022-07-08 13:59:07 +0900

    Modify : 글로번 폰트 link로 수정, variable.js 추가

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,25 +1,20 @@
-import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
-
   * {
     box-sizing: border-box; 
   }
 
   html {
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'Noto Sans KR', sans-serif; 
   }
   
   a, button {
      cursor: pointer;
   }
-
-
 
 `;
 
